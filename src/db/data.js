dec71d442f9bd3098c6898affad367d8988a43bc
Yinghao Wang <yinghao.a.wang@gmail.com>
2022-12-20 18:53:30 -0800

    random data generation added

---
+++ src/db/data.js
@@ -0,0 +1,58 @@
+import { generateRandomName } from "../util/util";
+
+// Generate fake testing data
+const generateRandomData = () => {
+    // Generate global user data
+    const userData = [];
+    const userLength = Math.floor(Math.random() * 100 + 10);
+    for (let userIndex = 0; userIndex < userLength; userIndex++) {
+        userData.push({name: generateRandomName(Math.random() * 4 + 4)});
+    }
+
+    // Generate global server data
+    const serverData = [];
+    for (let serverIndex = 0; serverIndex < Math.random() * Math.random() * 20; serverIndex++) {
+        const serverName = generateRandomName(8);
+
+        // Populate server with random users from users pool
+        const serverUsers = [];
+        for (let i = 0; i < userData.length; i++) {
+            if (Math.random() < .2) serverUsers.push(userData[i]);
+        }
+        if (userData.length === 0) {
+            const randomUserIndex = Math.floor(Math.random() * userLength);
+            serverUsers.push(userData[randomUserIndex]);
+        }
+
+        // Generate random channel data for each server
+        const channelData = [];
+        const channelLength = Math.random() * 5 + Math.random() * 3;
+        for (let channelIndex = 0; channelIndex < channelLength; channelIndex++) {
+            const channelName = generateRandomName(Math.random() * 5 + 3);
+
+            // Generate random post data for each channel
+            const postData = [];
+            for (let i = 0; i < Math.random() * 50 + 10; i++) {
+                const randomServerUserIndex = Math.floor(Math.random() * serverUsers.length);
+                postData.push({user: serverUsers[randomServerUserIndex], post: {content: `Hello to channel ${channelName} in ${serverName}`}});
+            }
+
+            channelData.push({
+                name: channelName,
+                posts: postData
+            });
+        }
+
+        serverData.push({
+            id: serverIndex,
+            name: serverName,
+            users: serverUsers,
+            channels: channelData
+        });
+    }
+
+    return { serverData, userData };
+}
+const { serverData, userData } = generateRandomData();
+
+export { serverData, userData };
---
+++ src/util/util.js
@@ -0,0 +1,10 @@
+const generateRandomName = nameLength => {
+    let res = '';
+    for(let i = 0; i < nameLength; i++){
+        const random = Math.floor(Math.random() * 26);
+        res += String.fromCharCode('a'.charCodeAt(0) + random);
+    };
+    return res;
+  };
+
+  export { generateRandomName };
