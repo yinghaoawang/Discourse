20bb4567b660911d43903fa42530ba88f85f8b4f
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-03-03 21:12:22 -0800

    frontend working with hosting

--- client/src/contexts/socket.context.js
+++ client/src/contexts/socket.context.js
@@ -6,7 +6,16 @@ export const SocketContext = createContext({
 });
 
 export const SocketProvider = ({ children }) => {
-    const socket = io(process.env.REACT_APP_SOCKET_URL);
+    let url = 'localhost:1250';
+    let options = {};
+    if (process.env.NODE_ENV === 'production') {
+        url = process.env.REACT_APP_SOCKET_URL;
+        options = {
+            path: process.env.REACT_APP_SOCKET_PATH,
+            secure: process.env.REACT_APP_SOCKET_SECURE
+        };
+    }
+    const socket = io(url, options);
     const value = { socket };
     return <SocketContext.Provider value={ value }>{ children }</SocketContext.Provider>
 }
