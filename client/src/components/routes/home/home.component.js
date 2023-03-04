a4c024cb684af3501ca67bdc8d8df9f68a25d3de
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-03-03 21:50:02 -0800

    user names added

--- client/src/components/routes/home/home.component.js
+++ client/src/components/routes/home/home.component.js
@@ -3,11 +3,15 @@ import './home.styles.scss';
 const Home = () => {
     return (
         <div className='home-container'>
-            <div className='content'>
-                <h1 className='header'>What's New in Discourse</h1>
-                <li>All modules refactored completely</li>
-                <li>Data loading for servers</li>
+            <div className='inner-container'>
+                <div className='content'>
+                    <h1 className='header'>What's New in Discourse</h1>
+                    <li>User names added</li>
+                    <li>Real time chat working</li>
+                    <li>All modules refactored completely</li>
+                </div>
             </div>
+            
         </div>
     );
 }
